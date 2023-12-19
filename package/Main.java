9f91f1ea9f4a0a70c7e922035569b99d566040cd
yevhenkushnir1993 <72440205+yevhenkushnir1993@users.noreply.github.com>
2023-12-19 21:34:13 +0200

    homework

---
+++ package/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+
+        double C = 10;
+        double F = (C * 9 / 5) + 32;
+        double K = C + 273.16;
+        System.out.println(C + " Celsius: " + F + " Fahrenheit: " + K + " Kelvin: ");
+
+        double a = 10;
+        double b = 15;
+
+        System.out.println("Площа прямокутного трикутника = " + a*b/2);
+
+
+
+
+
+
+    }
+}
