b3b42cfd0867cc4a80235351ef82fb4f8b165f3d
yevhenkushnir1993 <genyakyshnir@gmail.com>
2023-12-20 16:41:52 +0200

    homework4.1

---
+++ HW4/HW4.java
@@ -0,0 +1,14 @@
+public class HW4 {
+        public static void main(String[] args) {
+            System.out.println("Hello");
+
+            double C = 10;
+            double F = (C * 9 / 5) + 32;
+            double K = C + 273.16;
+            System.out.println(C + " Celsius: " + F + " Fahrenheit: " + K + " Kelvin: ");
+
+            double a = 10;
+            double b = 15;
+            System.out.println("Площа прямокутного трикутника = " + a*b/2);
+        }
+    }
